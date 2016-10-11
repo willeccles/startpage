314d714b900612414b316d2b59ebe4782cc1e1da
Will Eccles <willeccles860+github@gmail.com>
2016-10-11 14:59:16 -0400

    Started new startpage

---
+++ table.js
@@ -0,0 +1,22 @@
+var siteLinks = {
+	"column one" : {
+		"reddit" : "https://www.reddit.com",
+		"twitter" : "https://www.twitter.com"
+	},
+	"column two" : {
+		"apple" : "http://apple.com",
+		"google" : "http://google.com"
+	}
+};
+
+var table = "<table id=\"linktable\" class=\"linktable\">";
+
+function loadLinks() {
+	for (var key in siteLinks) {
+		// key = "column number"
+		// siteLinks[key] = {'page':'address'}
+		for (var link in siteLinks[key]) {
+		
+		}
+	}
+}

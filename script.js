7b70e3d5f85f867b72294a13ea4c0054171a442c
Will Eccles <willeccles860+github@gmail.com>
2017-07-01 10:40:49 -0400

    added mustard command

--- script.js
+++ script.js
@@ -290,6 +290,9 @@ function parseCom(com) {
 	else if (/^about:[A-Za-z0-9_-]+$/i.test(com)) {
 		document.location.href = com;
 	}
+	else if (/^(ttv\s)?(donny|mustard|bm)/.test(com)) {
+		nav("https://twitch.tv/barbaricmustard");
+	}
 	// These are some commands that are just for fun, and probably won't be added to the list
 	else if (/^cout << .*$/i.test(com)) {
 		var message = com.replace(/^cout << /i, "");
